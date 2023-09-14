89c0a34045802fbe8c706d3b56f2f9af0268725d
maribora28 <144677178+maribora28@users.noreply.github.com>
2023-09-14 14:42:22 +0300

    Update and rename 2.12.pas to 3.15pas

--- 2.12.pas
+++
@@ -1,7 +0,0 @@
-﻿program p12; 
-var a:integer; 
-begin 
-   writeln('введите четырёхзначное число'); 
-   readln(a); 
-   writeln((a mod 10)+(a div 10 mod 10)+(a div 100 mod 10)+(a div 1000)); 
-end.
