5c7b0e51caa6dfd932c32165600b0cb90a57daaa
maribora28 <144677178+maribora28@users.noreply.github.com>
2023-09-14 14:46:08 +0300

    Add files via upload

---
+++ 4.1.pas
@@ -0,0 +1,12 @@
+﻿program p41;
+var a, b,c,d:integer;
+begin
+  writeln ('Введите четырехзначное число');
+  readln(a);
+  b:= a mod 10;
+  a:=a div 10;
+  d:= a mod 10;
+  b:=10*b+d;
+  if a=b then writeln ('да')
+  else writeln('нет');
+  end.
