a44bb495cc5bb9bb981baa4aaab248b3004bbeb9
maribora28 <144677178+maribora28@users.noreply.github.com>
2023-09-21 09:56:50 +0300

    Add files via upload

---
+++ 4.21.pas
@@ -0,0 +1,14 @@
+﻿program p421;
+var a, b, c, d: integer;
+begin
+ writeln ('введите 3 числа');
+readln (a, b, c);
+d := 0;
+if ((a mod 2 = 0) or (b mod 2 = 0) or (c mod 2 = 0)) then
+d:=d+1;
+if ((a mod 2 <> 0) or (b mod 2 <> 0) or (c mod 2 <> 0)) then
+d:=d+1;
+if d >= 2
+then writeln ('да')
+else writeln ('нет');
+end.
