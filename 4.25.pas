7fc11fa776422f21ebc5b680d54137a3390a4304
maribora28 <144677178+maribora28@users.noreply.github.com>
2023-09-21 10:13:16 +0300

    Add files via upload

---
+++ 4.25.pas
@@ -0,0 +1,24 @@
+﻿program p425;
+
+var
+  a, b, c, d, e, f: integer;
+
+begin
+  writeln('Стоимость товара:');
+  writeln('руб. = '); 
+  writeln('коп. = ');
+  readln(a);
+  readln(b);
+  writeln('оплата: ', a, '-руб., ', b, 'коп.');
+  writeln('оплата: ');
+  write('руб. = '); readln(c);
+  write('коп. = '); readln(d);
+  writeln('проверка: ');
+  e := c - a;  f := d - b;
+  if f < 0 then begin e := e - 1; f := f + 100; end;
+  if e < 0 then writeln('не достаточно') else
+  begin
+    writeln('оплата: ', c, '-руб., ', d, '-коп.');
+    writeln('сдача: ', e, '-руб., ', f, '-коп.')
+  end;
+end.
